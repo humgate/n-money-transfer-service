8bb3791a0d847e20e0c9783238f5d51eefe6cb06
Pavel Yanovsky <humgate@gmail.com>
2022-04-03 23:38:24 +0300

    минорные правки в jabvadoc

--- src/main/java/com/humga/moneytransferservice/aspect/LogAspect.java
+++ src/main/java/com/humga/moneytransferservice/aspect/LogAspect.java
@@ -7,25 +7,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-
+/**
+ * Aspect определяет правила генерации и формирования содержания сообщений в лог консоли, в дополнении к
+ * стандартному логу консоли Spring
+ *
+ */
 @Aspect
 @Component
 public class LogAspect {
     //логер
     private static final Logger LOG = LoggerFactory.getLogger(LogAspect.class);
 
-    //логируем все методы контроллеров пакета controller
+    //логируем все методы пакета controller
     @Pointcut("within(com.humga.moneytransferservice.controller..*)")
     private void everythingInMyApplication() {}
 
-    //Перед выполнением метода логируем аргументы, то есть входной DTO из реквеста
+    //Перед выполнением метода логируем имя метода и аргументы, то есть входной DTO реквеста
     @Before("com.humga.moneytransferservice.aspect.LogAspect.everythingInMyApplication()")
     public void logMethodName(JoinPoint joinPoint) {
         LOG.info("Called {}", joinPoint.getSignature().getName() +
                 ": " + Arrays.stream(joinPoint.getArgs()).reduce((a,b) -> a+b.toString()));
     }
 
-    //После возврата из метода логируем возвращенный DTO
+    //После возврата из метода логируем имя метода и возвращенный DTO
     @AfterReturning(pointcut="com.humga.moneytransferservice.aspect.LogAspect.everythingInMyApplication()",
             returning="retVal")
     public void logReturn(JoinPoint joinPoint, Object retVal) {
