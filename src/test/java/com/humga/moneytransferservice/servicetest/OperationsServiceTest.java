cb0ed368a2022d576a6f738912670d2f8d37934b
Pavel Yanovsky <humgate@gmail.com>
2022-04-06 00:48:46 +0300

    Unauthorized transferTest

--- src/test/java/com/humga/moneytransferservice/servicetest/OperationsServiceTest.java
+++ src/test/java/com/humga/moneytransferservice/servicetest/OperationsServiceTest.java
@@ -1,22 +1,46 @@
 package com.humga.moneytransferservice.servicetest;
 
+import com.humga.moneytransferservice.exceptions.UnauthorizedException;
 import com.humga.moneytransferservice.model.*;
 import com.humga.moneytransferservice.repository.TransactionLog;
 import com.humga.moneytransferservice.service.AcquiringService;
 import com.humga.moneytransferservice.service.OperationsService;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.*;
 
 public class OperationsServiceTest {
+    static TransferRequestDTO reqDTO;
+    //класс простейший, поэтому в тесте будет работать его реальный экземпляр, не мок
+    static TransactionLog transactionLog;
+
+    @BeforeAll
+    public static void initSuite() {
+        reqDTO = new TransferRequestDTO();
+        transactionLog = new TransactionLog();
+
+        //входной параметр для тестируемого метода
+        reqDTO.setCardFromNumber(1111_1111_1111_1111L);
+        reqDTO.setCardToNumber(1111_1111_1111_1111L);
+        reqDTO.setCardFromCVV("111");
+        TransferRequestDTO.AmountDto amountDto = new TransferRequestDTO.AmountDto();
+        amountDto.setValue(1000);
+        amountDto.setCurrency("RUR");
+        reqDTO.setAmount(amountDto);
+        reqDTO.getAmount().setCurrency("RUR");
+    }
+
     /**
      * Тест метода transfer при успешном выполнении перевода
      *
      */
     @Test
+    @DisplayName("Authorized transfer test")
     void transferTest() {
         //given:
         //mock для ipsp
@@ -27,24 +51,6 @@ public class OperationsServiceTest {
 
         when(ipsp.verifyConfirmationCode(isA(String.class),isA(String.class))).thenReturn(true);
 
-        //mock для transaction
-        Transaction transaction = mock(Transaction.class);
-        when(transaction.getOperationId()).thenReturn(1L);
-
-        //класс простейший, поэтому в тесте будет работать его реальный экземпляр, не мок
-        TransactionLog transactionLog = new TransactionLog();
-
-        //входной параметр для тестируемого метода
-        TransferRequestDTO reqDTO = new TransferRequestDTO();
-        reqDTO.setCardFromNumber(1111_1111_1111_1111L);
-        reqDTO.setCardToNumber(1111_1111_1111_1111L);
-        reqDTO.setCardFromCVV("111");
-        TransferRequestDTO.AmountDto amountDto = new TransferRequestDTO.AmountDto();
-        amountDto.setValue(1000);
-        amountDto.setCurrency("RUR");
-        reqDTO.setAmount(amountDto);
-        reqDTO.getAmount().setCurrency("RUR");
-
         //экземпляр сервиса
         OperationsService service = new OperationsService(transactionLog, ipsp);
 
@@ -52,6 +58,32 @@ public class OperationsServiceTest {
         Response200DTO respDTO = service.transfer(reqDTO);
 
         //then:
-        Assertions.assertEquals(respDTO.getOperationId(),"1");
+        assertEquals("1",respDTO.getOperationId());
+    }
+
+    /**
+     * Тест метода transfer при отказе авторизации сервисом ipsp
+     *
+     */
+    @Test
+    @DisplayName("Unauthorized transfer test")
+    void transferUnauthorizedTest(){
+        //given:
+        //mock для ipsp
+        AcquiringService ipsp = mock(AcquiringService.class);
+
+        when(ipsp.authorizeTransaction(isA(TransactionAuthorizationRequest.class))).
+                thenReturn(AuthorizationStatus.UNAUTHORIZED);
+
+        when(ipsp.verifyConfirmationCode(isA(String.class),isA(String.class))).thenReturn(true);
+
+        //экземпляр сервиса
+        OperationsService service = new OperationsService(transactionLog, ipsp);
+
+        //when:
+        UnauthorizedException ex = assertThrows(UnauthorizedException.class,()->service.transfer(reqDTO));
+
+        //then:
+        assertEquals("Отказ. Карта/карты не авторизованы.", ex.getMessage());
     }
 }
