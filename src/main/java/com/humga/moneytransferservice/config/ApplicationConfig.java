ce2e276368f94c48fda7f0afd6526c1bad62c578
Pavel Yanovsky <humgate@gmail.com>
2022-05-28 15:44:08 +0300

    Исправлены замечания 2

--- src/main/java/com/humga/moneytransferservice/config/ApplicationConfig.java
+++ src/main/java/com/humga/moneytransferservice/config/ApplicationConfig.java
@@ -23,7 +23,7 @@ public class ApplicationConfig {
     public static class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
+            http.csrf().disable().authorizeRequests().anyRequest().permitAll();
         }
     }
 }
